8529f3e2f12e57232da145e7c9ddd7792341ca93
Anas-code1 <j79729091@gmail.com>
2026-03-09 13:54:20 +0500

    fix vercel deployment

--- rift-com/src/apis/index.js
+++ rift-com/src/apis/index.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
-const client = axios.create({ baseURL: 'http://localhost:4000' });
+const client = axios.create({ 
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:4000' 
+});
 
 export default client;
